1a156f616a904f5356e8faca8e576f20d896f352
Marijs Biebuyck <marijs.biebuyck@telenet.be>
2025-05-24 15:17:56 +0200

    afbeeldingen

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['scontent-bru2-1.xx.fbcdn.net', 'graph.mapillary.com'],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: ['images.mapillary.com'], // ✅ hier toestaan
+  },
+};
 
+module.exports = nextConfig;
